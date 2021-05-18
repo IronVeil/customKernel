9ffef0bed4d219bc67d35ac5184283b1ffab3865
IronVeil <jamiedanielking@gmail.com>
2021-05-18 06:09:38 +0100

    Made Communist propaganda lmao

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -2,6 +2,11 @@
 
 void kernel_main() {
     print_clear();
-    print_set_color(PRINT_COLOR_GREEN, PRINT_COLOR_BLACK);
-    print_str("Welcome to SwagOS");
+    print_set_color(PRINT_COLOR_RED, PRINT_COLOR_BLACK);
+    print_str("CommunismOS\n");
+    print_str("\nCommunism Pog\n");
+    print_set_color(PRINT_COLOR_BLUE, PRINT_COLOR_BLACK);
+    print_str("Capitalism cringe\n");
+    print_set_color(PRINT_COLOR_BLACK, PRINT_COLOR_GREEN);
+    print_str("\nMade by a lonely 15 year old :)\n");
 }
